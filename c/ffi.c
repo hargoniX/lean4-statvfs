35e1ee12547d553f24b272e0805b7405fe0d5795
Henrik Boeving <boeving@hm.edu>
2021-10-16 16:44:03 +0200

    Documentation for the ffi code

--- c/ffi.c
+++ c/ffi.c
@@ -9,31 +9,59 @@
 
 typedef struct statvfs statvfs_t;
 
+/**
+ * External class for `Statvfs`.
+ *
+ * This class registers `statvfs *` as a lean external class.
+ */
 static lean_external_class *g_statvfs_external_class = NULL;
 
+/**
+ * `lean_object *`(`Statvfs`) -> `statvfs *` conversion
+ */
 statvfs_t *statvfs_unbox(lean_object *s)
 {
     return (statvfs_t *)(lean_get_external_data(s));
 }
 
+/**
+ * `statvfs *` -> `lean_object *`(`Statvfs`) conversion
+ */
 lean_object *statvfs_box(statvfs_t *s)
 {
     return lean_alloc_external(g_statvfs_external_class, s);
 }
 
+/**
+ * A do nothing iterator.
+ */
 inline static void noop_foreach(void *mod, b_lean_obj_arg fn) {}
 
+/**
+ * `Statvfs` destructor.
+ */
 inline static void statvfs_finalizer(void *stat)
 {
     free((statvfs_t *)stat);
 }
 
+/**
+ * Initialize statvfs environment.
+ *
+ * This function does the following things:
+ * 1. register (`Statvfs`)`g_statvfs_external_class` class
+ *
+ * This function should always be called with `builtin_initialize`.
+ */
 lean_obj_res lean_statvfs_initialize()
 {
     g_statvfs_external_class = lean_register_external_class(statvfs_finalizer, noop_foreach);
     return lean_io_result_mk_ok(lean_box(0));
 }
 
+/**
+ * constant Statvfs.of_path (a : @& FilePath) : IO Statvfs
+ */
 lean_obj_res lean_statvfs_of_path(b_lean_obj_arg a, lean_obj_arg w)
 {
     const char *path = lean_string_cstr(a);
@@ -47,10 +75,16 @@ lean_obj_res lean_statvfs_of_path(b_lean_obj_arg a, lean_obj_arg w)
     }
 }
 
+/**
+ * `lean_object *`(`IO.FS.Handle`) -> `FILE *` conversion
+ */
 FILE *io_unbox_handle(lean_object *h) {
     return (FILE *)(lean_get_external_data(h));
 }
 
+/**
+ * constant Statvfs.of_handle (a : @& IO.FS.Handle) : IO Statvfs
+ */
 lean_obj_res lean_statvfs_of_handle(b_lean_obj_arg a, lean_obj_arg w)
 {
     FILE *fp = io_unbox_handle(a);
@@ -65,58 +99,90 @@ lean_obj_res lean_statvfs_of_handle(b_lean_obj_arg a, lean_obj_arg w)
 }
 
 
+/**
+ * constant Statvfs.bsize (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_bsize(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_bsize;
 }
 
+/**
+ * constant Statvfs.frsize (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_frsize(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_frsize;
 }
 
+/**
+ * constant Statvfs.fsid (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_fsid(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_fsid;
 }
 
+/**
+ * constant Statvfs.flag (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_flag(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_flag;
 }
 
+/**
+ * constant Statvfs.namemax (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_namemax(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_namemax;
 }
 
+/**
+ * constant Statvfs.blocks (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_blocks(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_blocks;
 }
 
+/**
+ * constant Statvfs.bfree (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_bfree(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_bfree;
 }
 
+/**
+ * constant Statvfs.bavail (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_bavail(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_bavail;
 }
 
+/**
+ * constant Statvfs.files (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_files(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_files;
 }
 
+/**
+ * constant Statvfs.ffree (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_ffree(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_ffree;
 }
 
+/**
+ * constant Statvfs.favail (a : @& Statvfs) : UInt64
+ */
 uint64_t lean_statvfs_favail(b_lean_obj_arg a, lean_obj_arg w)
 {
     return statvfs_unbox(a)->f_favail;
 }
-
